d234ea4a8692eb31e5d8bb80dd5ce1bf03af9e1b
Mindaugas K <mksolemn@gmail.com>
2019-11-26 12:21:01 +0100

    git Fixed generation

--- src/hello/index.ts
+++ src/hello/index.ts
@@ -1,13 +1,11 @@
 import {apply, mergeWith, Rule, SchematicContext, template, Tree, url} from '@angular-devkit/schematics';
-import {HelloSchematics} from './schema';
+import {Schema} from './schema';
 import {strings} from '@angular-devkit/core';
 
 
 // You don't have to export the function as default. You can also have more than one rule factory
 // per file.
-export function hello(_options: HelloSchematics): Rule {
-
-    // @ts-ignore
+export function hello(_options: Schema): Rule {
     return (tree: Tree, _context: SchematicContext) => {
                const sourceTemplates = url('./templates');
                const sourceParametrizedTemplates = apply(sourceTemplates, [
