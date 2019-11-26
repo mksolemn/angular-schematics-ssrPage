bc450648e479d0ba043090b87b8855cf1e7e9ec3
Mindaugas K <mksolemn@gmail.com>
2019-11-26 17:14:27 +0100

    Package can have only one generatable schematic, cuz in package.json you need to provide schematics name

---
+++ hello-page-demo-schematics/hello-page-demo-schematics.module.ts
@@ -0,0 +1,10 @@
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {classify} from '@angular-devkit/core/src/utils/strings';
+
+@NgModule({
+    declarations: [],
+    imports: [CommonModule]
+})
+export class PageDemoSchematicsModule {
+}
