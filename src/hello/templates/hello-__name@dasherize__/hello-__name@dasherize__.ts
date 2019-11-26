6667c5cb9c8f2bda3e43492aa69dd47e120fb663
Mindaugas K <mksolemn@gmail.com>
2019-11-26 16:34:57 +0100

    Working gen

--- src/hello/templates/hello-__name@dasherize__/hello-__name@dasherize__.ts
+++ src/hello/templates/hello-__name@dasherize__/hello-__name@dasherize__.ts
@@ -1,1 +1,11 @@
+import {Component} from '@angular/core'
+import {classify, dasherize} from '@angular-devkit/core/src/utils/strings';
+@Component({
+    selector: 'app-<%= dasherize(name) %>',
+    templateUrl: './<%= dasherize(name) %>.component.html',
+    styleUrls: ['./<%= dasherize(name) %>.component.css']
+})
+export class <%= classify(name) %>Component {
+    name = '<%= name %>';
+}
 console.log('Hello <%= name %>');
